938ace12b8b4c9daddaf814cf5566393e413d3fb
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2020-01-19 22:26:38 +0000

    upload 1.2
    Portfolio

--- script/main.js
+++ script/main.js
@@ -20,12 +20,12 @@ function myFunction() {
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var img = document.getElementById("myThumbnail");
+var pdfModal = document.getElementById("pdfReader");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = this.src;
+  pdfModal.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
