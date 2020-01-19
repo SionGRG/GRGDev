2ddd7771598342bc96ad4c3f4da4aa07557c558f
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2020-01-19 10:41:50 +0000

    upload .1.1
    portfolio

--- script/main.js
+++ script/main.js
@@ -9,9 +9,30 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky) {
     navbar.classList.add("stickyNavbar-menu")
   } else {
     navbar.classList.remove("stickyNavbar-menu");
   }
+}
+
+// Get the PDF modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
