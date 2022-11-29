dc5cb579c0c0a1f40b6f11b2e7d6a44f4ffdde4a
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2022-11-29 20:48:16 +0000

    Update 4.6
    + main pointer +
    * Bio *
    - added the cv as an embedded word document
    - working on fetching data from a json file
    
    * Portfolio *
    - corrected the error in embededDoc

---
+++ script/bio.js
@@ -0,0 +1,4 @@
+fetch( '../Data/Bio/txt.json' )
+const txt = '{"name":"John", "age":30, "city":"New York"}'
+const obj = JSON.parse(txt);
+document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
