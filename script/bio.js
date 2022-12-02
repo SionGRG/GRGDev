a69de1093c6fae4c3988e1008f435c3d97f3d2a9
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2022-12-02 23:50:54 +0000

    Update 5.1
    + main pointer +
    * Bio *
    - added the software stack data from the BioData json file
    - displayed the software stack data in a flex grid
    - removed the txt json file used for testing
    - added some software icons
    - added the language data from the Bio data, still need stilling

--- script/bio.js
+++ script/bio.js
@@ -8,25 +8,33 @@ LoadBioData().catch(function (err) {
 
 async function LoadBioData() {
     // fetch the json file
-    response = await fetch( "../../Data/Bio/txt.json" )
+    response = await fetch( "../Data/Bio/BioData.json" )
     if (response.status !== 200) {
         // For the github pages option
-        response = await fetch( "../GRGDev/Data/Bio/txt.json" );
+        response = await fetch( "../GRGDev/Data/Bio/BioData.json" );
     }
-
+    // Format into json data
     const data = await response.json();
-    // Display the json data
-    document.getElementById("demo").innerHTML = data.name + ", " + data.age;
-}
 
-/*
-fetch( '../Data/Bio/txt.json' )
-.then(response => response.json())
-.then(function (data) {
-    // Display the json data
-    document.getElementById("demo").innerHTML = data.name + ", " + data.age;
-})
-.catch(function (err) {
-    // handle errors
-    console.log(err)
-}); */
+    /* ***********************
+        Display the json data 
+    */
+    //  Display the technical skills
+
+    //  Display the software stack
+    let SW_Icon_Location = "files/images/bio/SWStack/";
+    let text_SWStack = "<div class='row c_SWStack'>";
+    for (let i = 0; i < data.SoftwareStack.length; i++) {
+        text_SWStack += "<div class='col " + data.SoftwareStack[i].class + "'><div class='card'><object class='card-img-top' data='" + SW_Icon_Location + data.SoftwareStack[i].icon + ".svg' alt='" + data.SoftwareStack[i].name + " icon'></object><div class='card-body'><h5 class='card-title'>" + data.SoftwareStack[i].name + "</h5><p class='card-text'>" + data.SoftwareStack[i].details + "</p></div></div></div>";
+    }
+    text_SWStack += "</div>";
+    document.getElementById("acr_SWStack").innerHTML = text_SWStack;
+
+    //  Display the languages
+    let text_lang = "<ul>";
+    for (let i = 0; i < data.Languages.length; i++) {
+        text_lang += "<li>" + data.Languages[i].name + "</li>";
+    }
+    text_lang += "</ul>";
+    document.getElementById("acr_Lang").innerHTML = text_lang;
+}
