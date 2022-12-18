29db5ba85d95efecf12f0d9364972e8456294e08
Sion GREG <45804912+SionGRG@users.noreply.github.com>
2022-12-18 21:22:48 +0000

    Update 5.3
    + main pointer +
    
    * Bio *
    - added the list elements method function to the grid class
    - renamed the grid class to make it more generic
    - added the language icons to the project

--- script/bio.js
+++ script/bio.js
@@ -1,5 +1,5 @@
 // Imports
-import {Grid_Cards} from './modules/grid-modules.js';
+import {Grid} from './modules/grid-modules.js';
 
 LoadBioData().catch(function (err) {
     // handle errors
@@ -18,24 +18,16 @@ async function LoadBioData() {
 
     /* ***********************
         Display the json data 
-    */
+    * ************************ */
     //  Display the technical skills
 
     //  Display the software stack
-    let SW_Grid = new Grid_Cards("files/images/bio/SWStack/", "row-cols-5 c_SWStack")
-    SW_Grid.UpdateElementsSVG(data.SoftwareStack);
-    SW_Grid.UpdateElementsIMG(data.SoftwareStack, "png");
-    SW_Grid.UpdateElementsIMG(data.SoftwareStack, "png");
-    SW_Grid.UpdateElementsIMG(data.SoftwareStack, "png");
-    SW_Grid.UpdateElementsIMG(data.SoftwareStack, "png");
-    SW_Grid.UpdateElementsIMG(data.SoftwareStack, "png");
+    let SW_Grid = new Grid("files/images/bio/SWStack/", "row-cols-5 c_SWStack")
+    SW_Grid.CreateCardElementsIMG(data.SoftwareStack, "png");
     SW_Grid.RenderOnId("acr_SWStack");
 
     //  Display the languages
-    let text_lang = "<ul>";
-    for (let i = 0; i < data.Languages.length; i++) {
-        text_lang += "<li>" + data.Languages[i].name + "</li>";
-    }
-    text_lang += "</ul>";
-    document.getElementById("acr_Lang").innerHTML = text_lang;
+    let Lang_Grid = new Grid("files/images/bio/Languages/", "row-cols-5 c_Languages");
+    Lang_Grid.CreateListElementsIMG(data.Languages, "png");
+    Lang_Grid.RenderOnId("acr_Lang");
 }
--- script/modules/grid-modules.js
+++ script/modules/grid-modules.js
@@ -1,13 +1,13 @@
 // A grid class that displays bootstrap cards as its elements
 
-export class Grid_Cards {
+export class Grid {
     constructor(thumbnailsLocation, gridClass) {
         this.m_ThumbnailsLocation = thumbnailsLocation;
         this.m_Output = "<div class='row " + gridClass + "'>";
     }
 
     // Add all the data from the json file with SVG thumbnails
-    UpdateElementsSVG(dataArray) {
+    CreateCardElementsSVG(dataArray) {
         for (let i = 0; i < dataArray.length; i++) {
             this.m_Output += "<div class='col " + dataArray[i].class + "'><div class='card'><object class='card-img-top' data='" + this.m_ThumbnailsLocation + dataArray[i].icon + ".svg' alt='" + dataArray[i].name + " icon'></object><div class='card-body'><h5 class='card-title'>" + dataArray[i].name + "</h5><p class='card-text'>" + dataArray[i].details + "</p></div></div></div>";
         }
@@ -16,9 +16,18 @@ export class Grid_Cards {
     /* Add all the data from the json file with image thumbnails i.e png, jpg, etc
      * the image format can be an fomart that is compatible with the html "img" tag
     */
-    UpdateElementsIMG(dataArray, imgFomart) {
+    CreateCardElementsIMG(dataArray, imgFomart) {
         for (let i = 0; i < dataArray.length; i++) {
             this.m_Output += "<div class='col " + dataArray[i].class + "'><div class='card'><img class='card-img-top' src='" + this.m_ThumbnailsLocation + dataArray[i].icon + "." + imgFomart + "' alt='" + dataArray[i].name + " icon'><div class='card-body'><h5 class='card-title'>" + dataArray[i].name + "</h5><p class='card-text'>" + dataArray[i].details + "</p></div></div></div>";
+        }
+    }
+
+    /* Add a list of items that have thumbnails in a grid 
+    * the image format can be an fomart that is compatible with the html "img" tag
+    */
+   CreateListElementsIMG(dataArray, imgFomart) {
+        for(let i = 0; i < dataArray.length; i++) {
+            this.m_Output += "<div class='col'><div><img class='w_icon' src='" + this.m_ThumbnailsLocation + dataArray[i].icon + "." + imgFomart + "' alt='" + dataArray[i].name + " icon'><p>" + dataArray[i].name + "</p></div></div>";
         }
     }
 
